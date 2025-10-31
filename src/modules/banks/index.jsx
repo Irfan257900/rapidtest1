16c845018e221af12dfa5a82070d4b32676d02f7
Mohammad Irfan <irfanmd40351@gmail.com>
2025-10-31 10:25:13 +0530

    Update index.jsx - banks transaction export feature

--- src/modules/banks/index.jsx
+++ src/modules/banks/index.jsx
@@ -1,28 +1,49 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { fetchKpis, resetState, fetchRecentActivityGraphData } from "../../reducers/banks.reducer";
 import { Outlet } from "react-router";
 import Overview from "./overview";
 import PageHeader from "../../core/shared/page.header";
+import CustomButton from "../../core/button/button";
+import TransactionExportModal from "./transaction.export.modal";
+
 const Banks = () => {
   const dispatch = useDispatch();
   const breadcrumb = useSelector((state) => state.banks.breadCrumbLists);
+  const [isExportModalOpen, setIsExportModalOpen] = useState(false);
+
   useEffect(() => {
     dispatch(fetchKpis({ showLoading: true }));
-    dispatch(fetchRecentActivityGraphData())
+    dispatch(fetchRecentActivityGraphData());
     return () => {
       dispatch(resetState());
     };
   }, []);
-  return (<>
-    <PageHeader
-      showBreadcrumb={true}
-      breadcrumbList={breadcrumb}
-    />
-    <Overview />
-    <Outlet />
-  </>);
 
+  const handleExportClick = () => {
+    setIsExportModalOpen(true);
+  };
+
+  const handleExportModalClose = () => {
+    setIsExportModalOpen(false);
+  };
+
+  return (
+    <>
+      <PageHeader showBreadcrumb={true} breadcrumbList={breadcrumb} />
+      <div className="flex justify-between items-center mb-4">
+        <Overview />
+        <CustomButton type="primary" onClick={handleExportClick}>
+          Export Transactions
+        </CustomButton>
+      </div>
+      <Outlet />
+      <TransactionExportModal
+        isOpen={isExportModalOpen}
+        onClose={handleExportModalClose}
+      />
+    </>
+  );
 };
 
 export default Banks;
